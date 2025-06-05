7e09b5c94f90f10d1f8819428a05f7db70af7a34
Deep Sharma <deepshar17@gmail.com>
2025-06-05 23:52:17 +0530

    add multi connection and room plus online count etc

--- main.go
+++ main.go
@@ -18,11 +18,19 @@ var upgrader = websocket.Upgrader{
 }
 
 type message struct {
+	Type     string `json:"type"`
+	Room     string `json:"room"`
 	Username string `json:"username"`
 	Content  string `json:"content"`
 	Online   int16  `json:"online"`
 }
+type Client struct {
+	conn     *websocket.Conn
+	username string
+	room     string
+}
 
+var rooms = make(map[string]map[*websocket.Conn]*Client)
 var Clients = make(map[*websocket.Conn]string)
 
 func ServeWs(w http.ResponseWriter, r *http.Request) {
@@ -32,8 +40,7 @@ func ServeWs(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer conn.Close()
-
-	var username string
+	var client *Client
 
 	Clients[conn] = ""
 	log.Default().Printf("New client connected: %s", conn.RemoteAddr().String())
@@ -50,23 +57,76 @@ func ServeWs(w http.ResponseWriter, r *http.Request) {
 			log.Default().Printf("Error while unmarshalling message: %v", err)
 		}
 
-		if Clients[conn] == "" {
-			Clients[conn] = msg.Username
+		if client == nil {
+			client = &Client{
+				conn:     conn,
+				username: msg.Username,
+				room:     msg.Room,
+			}
+
+			if rooms[client.room] == nil {
+				rooms[client.room] = make(map[(*websocket.Conn)]*Client)
+			}
+			rooms[client.room][conn] = client
+			continue
 		}
-		username = Clients[conn]
+
+		if msg.Type == "switchRoom" {
+			oldroom := client.room
+			delete(rooms[client.room], conn)
+			LeaveNotice := message{
+				Username: client.username,
+				Content:  "has leaved the room",
+				Online:   int16(len(Clients)),
+				Type:     "system",
+				Room:     oldroom,
+			}
+			outdata, _ := json.Marshal(LeaveNotice)
+			for conn := range rooms[oldroom] {
+				if err := conn.WriteMessage(messageType, outdata); err != nil {
+					log.Default().Printf("Error while writing message: %v", err)
+					conn.Close()
+					delete(Clients, conn)
+					return
+				}
+			}
+			if rooms[client.room] == nil || len(rooms[client.room]) == 0 {
+				delete(rooms, client.room)
+			}
+			client.room = msg.Room
+			if rooms[client.room] == nil {
+				rooms[client.room] = make(map[*websocket.Conn]*Client)
+			}
+			rooms[client.room][conn] = client
+
+			joinNotice := message{
+				Username: client.username,
+				Content:  "has joined the room",
+				Online:   int16(len(rooms[client.room])),
+				Type:     "system",
+				Room:     client.room,
+			}
+			outJoin, _ := json.Marshal(joinNotice)
+			for c := range rooms[client.room] {
+				c.WriteMessage(messageType, outJoin)
+			}
+		}
+
 		outgoin := message{
-			Username: username,
+			Username: msg.Username,
 			Content:  msg.Content,
 			Online:   int16(len(Clients)),
+			Type:     msg.Type,
+			Room:     msg.Room,
 		}
 		outdata, _ := json.Marshal(outgoin)
 
-		for client := range Clients {
+		for conn := range rooms[client.room] {
 
-			if err := client.WriteMessage(messageType, outdata); err != nil {
+			if err := conn.WriteMessage(messageType, outdata); err != nil {
 				log.Default().Printf("Error while writing message: %v", err)
-				client.Close()
-				delete(Clients, client)
+				conn.Close()
+				delete(Clients, conn)
 				return
 			}
 		}
